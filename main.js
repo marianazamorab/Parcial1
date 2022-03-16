6b98555546bc393eefdee865304f2ecadaf6a9c9
Mariana <m.zamorab@uniandes.edu.co>
2022-03-16 18:47:56 -0500

    2

--- main.js
+++ main.js
@@ -1,35 +1,149 @@
-const url = "https://gist.githubusercontent.com/josejbocanegra/9a28c356416badb8f9173daf36d1460b/raw/5ea84b9d43ff494fcbf5c5186544a18b42812f09/restaurant.json"
+const url =
+  "https://gist.githubusercontent.com/josejbocanegra/9a28c356416badb8f9173daf36d1460b/raw/5ea84b9d43ff494fcbf5c5186544a18b42812f09/restaurant.json";
 
 fetch(url)
-.then(response => response.json())
-.then(data => {
-    var html;
-    var html1;
-    var id=0;
+  .then((response) => response.json())
+  .then((data) => {
+    let html = "";
+    let productos = [];
+    const carrito = [];
+    let contador = 0;
 
-    data.forEach(element => {
-        if(element['name']!=""){
-        html+="<li class='nav-item'>"
-        html+="<button aria-current='page' type='button' class='botones' id='btn"+id+"' onclick=''>"+element['name']+"</a>"
-        html+="</li>"
-        }
-        document.querySelector('#nav1').innerHTML = html;
+    data.forEach((element) => {
+      const name = element["name"];
+      html += `<a class="nav-link active ${name}" href="#productos" id="btn${contador}">${name}</a>`;
+      productos[contador] = element["products"];
+      contador++;
+    });
 
+    let products = [];
 
-        let productos = element['products']
-
-        productos.forEach(e => {
-            html1+= '<div class="card" style="width: 18rem;">'+
-                '<div class="card-body">'+
-                '<h5 class="card-title">'+e['name']+'<h5>'+
-                '<p class="card-text">'+e['description']+'</p>'+
-                '<p class="card-text">'+e['price']+'</p>'+
-                '</div>'+'</div>'
+    productos.forEach(p => {
+        p.forEach(e => {
+            products.push(e);
         })
-     
-        document.querySelector('#btn'+id).setAttribute('onclick','document.querySelector("#central").innerHTML ="'+html1+'"')
-        html1="";
-        id++;
     })
-})
 
+    document.querySelector(".navbar-nav").innerHTML = html;
+
+    const navb = document.querySelector("#navb");
+
+    let cantidad = 0;
+
+    let html1 = "";
+
+    navb.addEventListener("click", (e) => {
+      if (e.target.classList.contains("Burguers")) {
+        console.log(e.parentElement)
+        html1 = "BURGUERS";
+        document.querySelector(".Roboto").innerHTML = html1;
+        html1 = "";
+        productos[0].forEach((p) => {
+          html1 +=
+            `<div class= "card col-lg-3 d-flex align-items-stretch justify-content-center">` +
+            `<div class="d-flex justify-content-center">` +
+            `<img src=${p["image"]} class="card-img-top"/>` +
+            `</div>` +
+            `<div class="card-body d-flex flex-column" id="${p["name"]}">` +
+            `<h5 class="card-title d-flex justify-content-center">${p["name"]}</h5>` +
+            `<p class="card-text">${p["description"]}</p>` +
+            `<p class="card-text"> $ ${p["price"]} </p>` +
+            `<a href="#" class="btn btn-primary d-flex justify-content-center">Add to cart</a>` +
+            `</div>` +
+            `</div>`;
+        });
+        document.querySelector("#productos").innerHTML = html1;
+      } else if (e.target.classList.contains("Tacos")) {
+        html1 = "TACOS";
+        document.querySelector(".Roboto").innerHTML = html1;
+        html1 = "";
+        productos[1].forEach((p) => {
+          html1 +=
+            `<div class= "card col-lg-3 d-flex align-items-stretch justify-content-center">` +
+            `<div class="d-flex justify-content-center">` +
+            `<img src=${p["image"]} class="card-img-top"/>` +
+            `</div>` +
+            `<div class="card-body d-flex flex-column" id="${p["name"]}">` +
+            `<h5 class="card-title d-flex justify-content-center">${p["name"]}</h5>` +
+            `<p class="card-text">${p["description"]}</p>` +
+            `<p class="card-text"> $ ${p["price"]} </p>` +
+            `<a href="#" class="btn btn-primary d-flex justify-content-center">Add to cart</a>` +
+            `</div>` +
+            `</div>`;
+        });
+        document.querySelector("#productos").innerHTML = html1;
+      } else if (e.target.classList.contains("Salads")) {
+        html1 = "SALADS";
+        document.querySelector(".Roboto").innerHTML = html1;
+        html1 = "";
+        productos[2].forEach((p) => {
+          html1 +=
+            `<div class= "card col-lg-3 d-flex align-items-stretch justify-content-center">` +
+            `<div class="d-flex justify-content-center">` +
+            `<img src=${p["image"]} class="card-img-top"/>` +
+            `</div>` +
+            `<div class="card-body d-flex flex-column align-items-stretch" id="${p["name"]}">` +
+            `<h5 class="card-title d-flex justify-content-center">${p["name"]}</h5>` +
+            `<p class="card-text">${p["description"]}</p>` +
+            `<p class="card-text"> $ ${p["price"]} </p>` +
+            `<a href="#" class="btn btn-primary d-flex justify-content-center">Add to cart</a>` +
+            `</div>` +
+            `</div>`;
+        });
+        document.querySelector("#productos").innerHTML = html1;
+      } else if (e.target.classList.contains("Desserts")) {
+        html1 = "DESSERTS";
+        document.querySelector(".Roboto").innerHTML = html1;
+        html1 = "";
+        productos[3].forEach((p) => {
+          html1 +=
+            `<div class= "card col-lg-3 d-flex align-items-stretch justify-content-center">` +
+            `<div class="d-flex justify-content-center">` +
+            `<img src=${p["image"]} class="card-img-top"/>` +
+            `</div>` +
+            `<div class="card-body d-flex flex-column align-items-stretch" id="${p["name"]}">` +
+            `<h5 class="card-title d-flex justify-content-center">${p["name"]}</h5>` +
+            `<p class="card-text">${p["description"]}</p>` +
+            `<p class="card-text"> $ ${p["price"]} </p>` +
+            `<a href="#" class="btn btn-primary d-flex justify-content-center">Add to cart</a>` +
+            `</div>` +
+            `</div>`;
+        });
+        document.querySelector("#productos").innerHTML = html1;
+      } else if (e.target.classList.contains("Drinks")) {
+        html1 = "DRINKS AND SIDES";
+        document.querySelector(".Roboto").innerHTML = html1;
+        html1 = "";
+        productos[4].forEach((p) => {
+          html1 +=
+            `<div class= "card col-lg-3 d-flex align-items-stretch justify-content-center">` +
+            `<div class="d-flex justify-content-center">` +
+            `<img src=${p["image"]} class="card-img-top"/>` +
+            `</div>` +
+            `<div class="card-body d-flex flex-column align-items-stretch" id="${p["name"]}">` +
+            `<h5 class="card-title d-flex justify-content-center">${p["name"]}</h5>` +
+            `<p class="card-text">${p["description"]}</p>` +
+            `<p class="card-text"> $ ${p["price"]} </p>` +
+            `<a href="#" class="btn btn-primary d-flex justify-content-center">Add to cart</a>` +
+            `</div>` +
+            `</div>`;
+        });
+        document.querySelector("#productos").innerHTML = html1;
+      }
+    });
+
+    const cant = document.querySelector("#cantidad");
+
+    const prod = document.querySelector("#productos");
+
+    prod.addEventListener("click", (e) => {
+      if (e.target.classList.contains("btn")) { 
+        var prod = products.filter(
+          (p) => p["name"] === e.target.parentElement.id
+        );
+        carrito.push(prod);
+        if(carrito.(prod)){ cantidad++; }
+        cant.textContent = cantidad;
+      }
+    });
+  });
